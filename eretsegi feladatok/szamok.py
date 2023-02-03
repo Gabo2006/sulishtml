33b2ea31aaea8873d55799aaee87be52c3037d95
Kiss Gábor <kissg21@ADY-NAGYATAD.SULINET.HU>
2023-02-03 09:39:26 +0100

    asd

--- eretsegi feladatok/szamok.py
+++ eretsegi feladatok/szamok.py
@@ -1,3 +1,5 @@
+import random
+
 f=open("felszam.txt","r")
 
 
@@ -14,3 +16,76 @@ f.close()
 
 print("2. feladat")
 print("Az adatfileban" + str(len(kerdesek)) + "kérdés van.")
+
+matek=[]
+for e in kerdesek:
+    if e[3]=="matematika":
+        matek.append(e[2])
+    
+print("Az adatfajlban "+ str(len(matek)) +" matematika feladat van, 1 pontot er "+ str(matek.count(1)) +" feladat, 2 pontot er "+ str(matek.count(2)) +" feladat, 3 pontot er "+ str(matek.count(3)) +" feladat.")
+
+valaszok=[]
+for e in kerdesek:
+    valaszok.append(e[1])
+
+
+valaszok=[e[1] for e in kerdesek]
+print("A válaszok számértéke {}-től {}-ig tart.".format(min(valaszok),max(valaszok)))
+
+
+temakorok=[]
+for e in kerdesek:
+    if e[3] not in temakorok:
+        temakorok.append(e[3])
+
+print("5.feladat")
+
+print("A témakörök: "+ ", ".join(temakorok))
+
+print("6. feledat")
+beker=input("Milyen témakorből szeretne kérdést kapni? ")
+
+ujLista=[e for e in kerdesek if e[3]==beker]
+
+sorsolt=random.choice(ujLista)
+
+valasz=input(sorsolt[0])
+if int(valasz)==sorsolt[1]:
+    print(str(sorsolt[2])+"pont")
+else:
+    print("0 pont")
+    print("a helyes valasz:"+str(sorsolt[1]))
+
+
+
+
+print("7. feladat")
+
+lista10=[]
+for i in range(10):
+    r=random.choice(kerdesek)
+    while r in lista10:
+          r=random.choice(kerdesek)
+    lista10.append(r)
+
+print(lista10)
+
+random.shuffle(kerdesek)
+lista10=kerdesek[0:10]
+
+print(len(lista10))
+
+
+f=open("tesztfel.txt","w")
+ossz=0
+for e in lista10:
+    f.write(str(e[2])+" "+str(e[1])+" "+str(e[0])+"\n") 
+    ossz+=e[2]
+
+
+
+
+f.write("A feladatsorra összesen {0} pont adható".format(ossz))
+
+f.close()
+
