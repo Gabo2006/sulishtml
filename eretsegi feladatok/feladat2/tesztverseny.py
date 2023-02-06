39ba4944ff2c8b851b1af3ee51920ef64f0c702a
Kiss Gábor <kissg21@ADY-NAGYATAD.SULINET.HU>
2023-02-06 08:44:50 +0100

    asd

---
+++ eretsegi feladatok/feladat2/tesztverseny.py
@@ -0,0 +1,24 @@
+#1 feladat
+f=open("valaszok.txt","r")
+
+adatok=f.read().split("\n")
+adatok.remove("")
+
+
+
+
+f.close()
+
+#print(adatok)
+
+
+helyes=adatok[0]
+adatok.remove(helyes)
+
+valaszok=[]
+for e in adatok:
+     valaszok.append(e.split(" "))
+   
+    
+
+print(valaszok)
